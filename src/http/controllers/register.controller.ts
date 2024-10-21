8f52b50a03cfd6c7b9c3bc5d76ccd2595a094380
ferreira.jim <jimdouglasmmferreira@gmail.com>
2024-10-21 17:45:12 -0300

    feat: creating interface for the repository

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterService } from '@/services/register.services'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
--- src/services/register.services.ts
+++ src/services/register.services.ts
@@ -1,5 +1,4 @@
-import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { IUsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterServiceRequest {
@@ -8,17 +7,13 @@ interface RegisterServiceRequest {
   password: string
 }
 export class RegisterService {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: IUsersRepository) {}
 
   async execute({ name, email, password }: RegisterServiceRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
-
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+    
     if (userWithSameEmail) throw new Error('Email already exists.')
 
     await this.usersRepository.create({ 
