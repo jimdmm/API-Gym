566e7930205b5eb92fd66874ed96d38807dfbda6
ferreira.jim <jimdouglasmmferreira@gmail.com>
2025-03-12 16:16:01 -0300

    feat: adding test e2e to user

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -6,7 +6,7 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
 
   const { user } = await getUserProfile.execute({ userId: request.user.sub })
 
-  return reply.status(200).send({
-    user: { ...user, password_hash: undefined, created_at: undefined },
-  })
+  return reply
+    .status(200)
+    .send({ ...user, password_hash: undefined, created_at: undefined })
 }
